86c93fb5f13f6a52b1f9d984be1a702b2e03aaad
msnasreena5-del <msnasreena5@gmail.com>
2026-01-20 22:35:14 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+color = input("What is your favorite color? ")
+
+
+print("\n Welcome", name + "! ")
+print(f"You are {age} years old.")
+print(f"You love {hobby} and your favorite color is {color}.")
+print("Thanks for sharing a bit about yourself")
